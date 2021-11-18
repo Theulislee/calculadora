a5ab85f6d387692b7f471818706f0cf5621d0c0c
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-11-18 16:24:30 -0300

    creation of a calculator module

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// creation of a calculator module / criação de modulo calculadora
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class CalculadoraModule { }
