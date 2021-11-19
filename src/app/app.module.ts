59d8e2fd0117b2788c4950c3ed5021d9670ef044
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-11-19 11:20:46 -0300

    add calculation module to componets.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora/calculadora.module';     // add calculadora.module.ts here
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule            //add Calculadora.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
